eb162dc2e3ae73a8bdbca34ff2e148f0c01d8682
Naor0147 <NAORAGAM0147@gmail.com>
2023-10-18 14:57:34 +0300

    final

--- Final_Project1/Classes/Ball.cs
+++ Final_Project1/Classes/Ball.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.Foundation;
 using Windows.UI;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Media.Animation;
 using Windows.UI.Xaml.Shapes;
-
+using System.Windows;
+using Windows.UI.Xaml;
+using System.Diagnostics;
 
 namespace Final_Project1.Classes
 {
@@ -24,7 +27,9 @@ namespace Final_Project1.Classes
 
         private Canvas canvas { get; set; }
 
-        public Ball(double Size, double x, double y, double vy, double vx, Canvas canvas)
+        private List<Rectangle> Borders { get; set; }
+
+        public Ball(double Size, double x, double y, double vy, double vx, Canvas canvas , List<Rectangle> Borders)
         {
             this.Size = Size;
             this.x = x;
@@ -32,6 +37,8 @@ namespace Final_Project1.Classes
             this.vy = vy;
             this.vx = vx;
             this.canvas = canvas;
+
+            this.Borders = Borders;
 
             Create_ball();
 
@@ -56,15 +63,73 @@ namespace Final_Project1.Classes
             this.y += this.vy;
             Canvas.SetLeft(ellipse, this.x);
             Canvas.SetTop(ellipse, this.y);
-            if (x >(canvas.ActualWidth*0.7-100) || x<0)//doesnt get the real height and width 
+            
+            Rect ellipseBounds = new Rect(new Point(Canvas.GetLeft(ellipse), Canvas.GetTop(ellipse))
+                , new Point(Canvas.GetLeft(ellipse) + ellipse.Width, Canvas.GetTop(ellipse) + ellipse.Height));
+            
+            
+            foreach (Rectangle rect in Borders)
+            {
+                // Get the positions and sizes of the rectangle and ellipse
+                // Corrected code to create Rect objects
+                Rect rectBounds = new Rect(new Point(Canvas.GetLeft(rect), Canvas.GetTop(rect)), new Point(Canvas.GetLeft(rect) + rect.Width, Canvas.GetTop(rect) + rect.Height));
+
+
+                // Check for collision by comparing the bounds of the rectangle and ellipse
+                //bool hasCollided = rectBounds.IntersectsWith(ellipseBounds);
+                Rect _rect = RectHelper.Intersect(ellipseBounds, rectBounds);
+                bool _rectHelp = AreRectanglesColliding(rectBounds, ellipseBounds);
+
+
+                if (_rect.Width > 0 || _rect.Height > 0)
+                {
+                    Debug.WriteLine(_rect.ToString());
+
+                    See_Collison(_rect);
+                    if (_rect.Width>_rect.Height)
+                    {
+                        this.vx = -vx;
+                    }
+                    else
+                    {
+                       this.vy = -vy;
+                    }
+
+                }
+
+
+            }
+
+
+
+
+        /*    if (x >(canvas.ActualWidth) || x<0)//doesnt get the real height and width 
             {
                 this.vx = -5;
             }
-            if (y>(canvas.ActualWidth*0.7-100)|| y<0)
+            if (y>(canvas.ActualWidth)|| y<0)
             {
                 this.vy = -5;
             }
-
+        */
         }
+
+        private void See_Collison(Rect _rect)
+        {
+            Rectangle boundaryRect = new Rectangle();
+            boundaryRect.Width = _rect.Width;  // Set the width to match the Rect
+            boundaryRect.Height = _rect.Height;  // Set the height to match the Rect
+            boundaryRect.Stroke = new SolidColorBrush(Colors.Red);  // Set the border color to red
+            boundaryRect.StrokeThickness = 2;
+            Canvas.SetLeft(boundaryRect, _rect.Left);
+            Canvas.SetTop(boundaryRect, _rect.Top);
+            canvas.Children.Add(boundaryRect);
+        }
+
+        public bool AreRectanglesColliding(Rect rect1, Rect rect2)
+        {
+            return (rect1.Left < rect2.Right && rect1.Right > rect2.Left && rect1.Top < rect2.Bottom && rect1.Bottom > rect2.Top);
+        }
+
     }
 }
--- Final_Project1/Pages/GamePage1.xaml.cs
+++ Final_Project1/Pages/GamePage1.xaml.cs
@@ -7,6 +7,7 @@ using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI;
+using Windows.UI.Core;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -14,6 +15,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Shapes;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -27,18 +29,39 @@ namespace Final_Project1.Pages
         public DispatcherTimer timer;
         public const double Gravity = 9.8;
         Ball _ball;
+        List<Rectangle> _Border_List;
         public GamePage1()
         {
             this.InitializeComponent();
-          
-            
+
+            //Get Borders 
+            Get_Borders();
 
             //Timer
+            Start_Timer();
+
+           
+
+        }
+
+        private void Get_Borders()
+        {
+            _Border_List = new List<Rectangle>();
+            var _border_List = Canvas_game.Children.OfType<Rectangle>().Where(rectangle => rectangle.Tag!= null &&rectangle.Tag.ToString() == "border_canvas");// gets all the border i the game page
+            int i = 0;
+            foreach (var _border in _border_List)
+            {
+                _Border_List.Add( _border);
+                i++;
+            }
+        }
+
+        private void Start_Timer()
+        {
             timer = new DispatcherTimer();
             timer.Interval = TimeSpan.FromMilliseconds(5);
-            timer.Tick += Timer_Tick; 
+            timer.Tick += Timer_Tick;
             timer.Start();
-            //
         }
 
         private void Timer_Tick(object sender, object e)
@@ -53,7 +76,25 @@ namespace Final_Project1.Pages
 
         private void Canvas_game_Loaded(object sender, RoutedEventArgs e)
         {
-            _ball = new Ball(100, 400, 400, 5, 5, Canvas_game);
+            _ball = new Ball(100, 400, 400, 4, 5, Canvas_game , _Border_List);
+
+        }
+
+        private void Page_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+
+            foreach(Rectangle rectangle in _Border_List)
+            {
+                if (rectangle.Width==1880)
+                {
+                    
+                    double calc = (_Grid.ActualHeight / 6 * 5) - 20 - 40 - 20;
+                    Canvas.SetTop(rectangle, calc);
+                    break;                 
+                }
+            }
+           
+            
         }
     }
 }
